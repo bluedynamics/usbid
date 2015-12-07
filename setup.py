99dd1cd7e6b19b44939ae9328a71729d540fd907
Robert Niederreiter <office@squarewave.at>
2015-12-07 13:34:33 +0100

    kick ipython from test requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ shortdesc = \
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
-tests_require = ['interlude', 'ipython']
+tests_require = ['interlude']
 
 
 setup(
